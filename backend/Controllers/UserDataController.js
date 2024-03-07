0da801ee149f41fb4a9b7ddea1bdfef8ff7b1213
vishalsharma070901 <shobits723@gmail.com>
2024-03-07 23:04:20 +0530

    Final Commit

--- backend/Controllers/UserDataController.js
+++ backend/Controllers/UserDataController.js
@@ -87,7 +87,7 @@ exports.sendData = asyncHandler(async (req, res) => {
         name: "CRUD Application",
         address: EMAIL,
       },
-      to: "info@redpositive.in", // list of receivers
+      to: "shobits723@gmail.com", // list of receivers
       subject: "User Data", // Subject line
       text: text, // plain text body
     };
